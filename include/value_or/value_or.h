83633c4814c37126f31ee9d284565388b15706d1
Pete Cordell <pete.git@codalogic.com>
2018-09-12 20:32:17 +0100

    End of first play

---
+++ include/value_or/value_or.h
@@ -0,0 +1,92 @@
+//----------------------------------------------------------------------------
+// Licensed under the MIT/X11 license - https://opensource.org/licenses/MIT
+//----
+// Copyright (c) 2018, Codalogic Ltd (http://www.codalogic.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//----------------------------------------------------------------------------
+
+#ifndef CODALOGIC_VALUE_OR
+#define CODALOGIC_VALUE_OR
+
+namespace valueor {
+
+//----------------------------------------------------------------------------
+// The default validators
+//----------------------------------------------------------------------------
+
+template< typename Tvalue >
+struct value_or_validator
+{
+	static bool is_valid( const Tvalue & v ) { return false; }
+};
+
+template< typename Tvalue >
+struct value_or_validator< Tvalue * >
+{
+	static bool is_valid( const Tvalue * const p_v ) { return p_v; }
+};
+
+//----------------------------------------------------------------------------
+// The functions
+//----------------------------------------------------------------------------
+
+template < typename Texception, typename Tvalue >
+const Tvalue & value_or( const Tvalue & v )
+{
+	if( ! value_or_validator<const Tvalue &>::is_valid( v ) )
+		throw Texception();
+	return v;
+}
+
+template < typename Texception, typename Tvalue >
+Tvalue & value_or( Tvalue & v )
+{
+	if( ! value_or_validator<const Tvalue &>::is_valid( v ) )
+		throw Texception();
+	return v;
+}
+
+template < typename Texception, typename Tvalue >
+const Tvalue * value_or( const Tvalue * p_v )
+{
+	if( ! value_or_validator<const Tvalue *>::is_valid( p_v ) )
+		throw Texception();
+	return p_v;
+}
+
+template < typename Texception, typename Tvalue >
+Tvalue * value_or( Tvalue * p_v )
+{
+	if( ! value_or_validator<const Tvalue *>::is_valid( p_v ) )
+		throw Texception();
+	return p_v;
+}
+
+//template < typename Texception >
+//int * value_or( int * p_v )
+//{
+//	if( ! value_or_validator<int *>::is_valid( p_v ) )
+//		throw Texception();
+//	return p_v;
+//}
+
+} // End of namespace valueor
+
+#endif	// CODALOGIC_VALUE_OR
---
+++ value_or_example.cpp
@@ -0,0 +1,156 @@
+#include "annotate-lite.h"
+using namespace annotate_lite;
+#include "include/value_or/value_or.h"
+using namespace valueor;
+
+#pragma warning( disable:4996 )
+
+#include <cstdio>
+#include <fstream>
+
+void pointer_types()
+{
+	Scenario( "Pointer Types" );
+
+	struct BadPointer1 {};
+	struct BadPointer2 {};
+	try
+	{
+		int i = 0;
+		int * p_i = value_or<BadPointer1>( &i );
+		Good( "Exception should not thrown" );
+		p_i = value_or<BadPointer2>( (int*)0 );
+		Bad( "Exception should have thrown" );
+	}
+	catch( const BadPointer1 & )
+	{
+		Bad( "Exception thrown" );
+	}
+	catch( const BadPointer2 & )
+	{
+		Good( "Exception thrown" );
+	}
+}
+
+void unknown_types()
+{
+	Scenario( "Unknown Types" );
+
+	struct BadUnknown {};
+	try
+	{
+		float f = value_or<BadUnknown>( 1.0f );
+		Bad( "Exception should have thrown" );
+	}
+	catch( const BadUnknown & )
+	{
+		Good( "Exception thrown" );
+	}
+}
+
+namespace valueor {
+	template<>
+	struct value_or_validator< const std::ifstream & >
+	{
+		static bool is_valid( const std::ifstream & v ) { return v.is_open(); }
+	};
+}
+
+void fopen_file_types()
+{
+	Scenario( "File Types - fopen" );
+
+	struct BadFile1 {};
+	struct BadFile2 {};
+	try
+	{
+		FILE * fin1 = value_or<BadFile1>( fopen( "value_or_example.cpp", "r" ) );
+		Good( "Exception not thrown" );
+		fclose( fin1 );
+
+		FILE * fin2 = value_or<BadFile2>( fopen( "Non-existent file", "r" ) );
+		Bad( "Exception should have thrown" );
+	}
+	catch( const BadFile1 & )
+	{
+		Bad( "Exception should have thrown" );
+	}
+	catch( const BadFile2 & )
+	{
+		Good( "Exception thrown" );
+	}
+}
+
+void fstream_file_types()
+{
+	Scenario( "File Types - ifstream" );
+
+	struct BadFile1 {};
+	struct BadFile2 {};
+	try
+	{
+		std::ifstream fin1( "value_or_example.cpp" );
+		value_or<BadFile1>( fin1 );
+		Good( "Exception not thrown" );
+
+		std::ifstream fin2( "non-existent file" );
+		value_or<BadFile2>( fin2 );
+		Bad( "Exception should have thrown" );
+	}
+	catch( const BadFile1 & )
+	{
+		Bad( "Exception should have thrown" );
+	}
+	catch( const BadFile2 & )
+	{
+		Good( "Exception thrown" );
+	}
+}
+
+template< typename Texception >
+class ifstream_or : public std::ifstream
+{
+public:
+	ifstream_or<Texception>( const char *_Filename,
+		ios_base::openmode _Mode = std::ios_base::in,
+		int _Prot = (int)std::ios_base::_Openprot )
+		:
+		std::ifstream( _Filename, _Mode, _Prot )
+	{
+		if( ! is_open() )
+			throw Texception();
+	}
+};
+
+void fstream_or_file_types()
+{
+	Scenario( "File Types - ifstream_or" );
+
+	struct BadFile1 {};
+	struct BadFile2 {};
+	try
+	{
+		ifstream_or<BadFile1> fin1( "value_or_example.cpp" );
+		Good( "Exception not thrown" );
+
+		ifstream_or<BadFile2> fin2( "non-existent file" );
+		Bad( "Exception should have thrown" );
+	}
+	catch( const BadFile1 & )
+	{
+		Bad( "Exception should have thrown" );
+	}
+	catch( const BadFile2 & )
+	{
+		Good( "Exception thrown" );
+	}
+}
+
+int main()
+{
+	pointer_types();
+	unknown_types();
+	fopen_file_types();
+	fstream_file_types();
+	fstream_or_file_types();
+}
